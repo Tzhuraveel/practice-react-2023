17576d67c95cb0f274ab3e616c1171c017b91796
Tzhuraveel <timofiyzhuravel2003@gmail.com>
2023-02-04 14:01:54 +0200

    practice-lesson-5

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
+import {Route, Routes, useNavigate} from "react-router-dom";
+import {UsersPage} from "./components/UsersPage";
 
 const App = () => {
+    const navigate = useNavigate();
     return (
         <div>
-
+            <div>
+                <button onClick={() => navigate('/users')}>users</button>
+                <button onClick={() => navigate('/cars')}>cars</button>
+            </div>
+            <hr/>
+            <Routes>
+                <Route path={'/users'} element={<UsersPage/>}/>
+            </Routes>
         </div>
     );
 };
