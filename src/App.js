2ab6331646976946518dfa65149a693c1ea54080
Tzhuraveel <timofiyzhuravel2003@gmail.com>
2023-02-05 12:45:12 +0200

    practice-lesson-5

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
+import {useDispatch, useSelector} from "react-redux";
+
 
 const App = () => {
+
+    const state = useSelector((state) => state);
+    const dispatch = useDispatch();
+
+
     return (
         <div>
+            <h1>{state}</h1>
+            <button onClick={() => dispatch({type: 'INC'})}>INCREMENT</button>
+            <button onClick={() => dispatch({type: 'DEC'})}>DECREMENT</button>
         </div>
     );
 };
--- src/index.js
+++ src/index.js
@@ -2,12 +2,33 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import {App} from './App';
-import {BrowserRouter} from "react-router-dom";
+
+
+import {Provider} from "react-redux";
+import {createStore} from "redux";
+
+const reducer = (state = 0, action) => {
+
+    switch (action.type){
+        case 'INC':
+            return ++state
+        case 'DEC':
+            return --state
+        default:
+            return state
+    }
+
+}
+
+const store = createStore(reducer);
+
+console.log(store)
+console.log(store.getState());
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <BrowserRouter>
+    <Provider store={store}>
         <App />
-    </BrowserRouter>
+    </Provider>
 );
 
